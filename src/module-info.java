c5aca942068895d359cd586b58f2815c1eed7974
Victor Castro <vgabriel2211@gmail.com>
2021-05-01 12:06:58 -0300

    MainViewController

--- src/module-info.java
+++ src/module-info.java
@@ -2,5 +2,8 @@ module workshop {
 	requires javafx.controls;
 	requires javafx.fxml;
 	
+	exports gui to javafx.fxml;
+	
 	opens application to javafx.graphics, javafx.fxml;
+	opens gui to javafx.graphics, javafx.fxml;
 }
